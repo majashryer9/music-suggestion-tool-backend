95fbc8a892c5a1bfed94d3ec3e9f3f059cb0c55f
Maja Shryer <majashryer@gmail.com>
2020-01-04 14:09:11 -0500

    Add elasticsearch and function to save song names with playlist id so in the future we may search for playlists by songs they contain.

--- src/daos/song-dao.ts
+++ src/daos/song-dao.ts
@@ -1,5 +1,21 @@
-import { IPlaylist } from "../models/Playlist";
+import { IPlaylist } from '../models/Playlist';
+import { client } from '../util/elasticsearch-util';
 
-export const saveSongsWithPlaylistId = (playlist: IPlaylist) => {
-
+// Save songs to elasticsearch so we can search for playlists by song name
+export const saveSongsWithPlaylistId = async (playlist: IPlaylist) => {
+    const body: Object[] = [];
+    playlist.songs.forEach(song =>
+        body.push(
+            { index: { _index: 'songs' } },
+            {
+                songName: song.songName,
+                playlistId: playlist.id
+            }
+        )
+    );
+    try {
+        client.bulk({ refresh: "true", body });
+    } catch (error) {
+        console.error(error);
+    }
 }
