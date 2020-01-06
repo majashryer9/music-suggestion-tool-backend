2067cc21edcf0712da989aa91cfebab323df70e7
Maja Shryer <majashryer@gmail.com>
2020-01-05 21:09:08 -0500

    Add checks on request bodies.

--- src/routers/playlist-router.ts
+++ src/routers/playlist-router.ts
@@ -1,9 +1,15 @@
 import Router, { Request, Response } from 'express';
 import * as playlistService from '../services/playlist-service';
+import { IPlaylist } from '../models/Playlist';
 
 export const playlistRouter = Router();
 
 playlistRouter.post('/save', async (req: Request, resp: Response) => {
-    const savedPlaylist = await playlistService.savePlaylist(req.body.playlist);
-    resp.json(savedPlaylist);
+    const playlist: undefined | IPlaylist = req.body.playlist;
+    if (playlist) {
+        const savedPlaylist = await playlistService.savePlaylist(req.body.playlist);
+        resp.json(savedPlaylist);
+    } else {
+        resp.status(400).json('Must include a playlist to save.')
+    }
 });
--- src/routers/song-router.ts
+++ src/routers/song-router.ts
@@ -4,11 +4,21 @@ import * as songService from '../services/song-service';
 export const songRouter = Router();
 
 songRouter.post('/get-related-songs', async (req: Request, resp: Response) => {
-    const relatedSongs = await songService.getRelatedSongs(req.body.spotifyTrackIds);
-    resp.json(relatedSongs);
+    const spotifyTrackIds: undefined | string[] = req.body.spotifyTrackIds;
+    if (Array.isArray(spotifyTrackIds)) {
+        const relatedSongs = spotifyTrackIds.length ? await songService.getRelatedSongs(req.body.spotifyTrackIds) : [];
+        resp.json(relatedSongs);
+    } else {
+        resp.status(400).json('Must include spotify track ids.');
+    }
 });
 
 songRouter.post('/spotify-song-search', async (req: Request, resp: Response) => {
-    const searchResults = await songService.spotifySongSearch(req.body.searchTerm);
-    resp.json(searchResults);
+    const searchTerm: undefined | string = req.body.searchTerm;
+    if (typeof searchTerm === 'string') {
+        const searchResults = searchTerm.length ? await songService.spotifySongSearch(req.body.searchTerm) : [];
+        resp.json(searchResults);
+    } else {
+        resp.status(400).json('Must include a search term.');
+    }
 });
